36e6fa9545fc8d31d2aa7f5052e4b7971c2e7c3e
luong minh trung <luongminhtrung1691994@gmail.com>
2018-03-07 00:17:08 +0700

    load api data

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -25,6 +25,31 @@ var slick = $(".wrap-slider-news__slider-container").slick({
     ]
 });
 
+var pageNumber = 1;
+var isLoading = false;
+slick.on("beforeChange", function (event, slick, currentSlide, nextSlide){
+    if ( (slick.currentSlide + slick.options.slidesToShow) == slick.slideCount - 1 && isLoading == false){
+        isLoading = true;
+        $.ajax({
+            url: "http://cybersecurityasean.com/web-api/index.php/json-items/5E9512E4431C5A3EB1D83627F08059C3/" + pageNumber,
+            method: "GET",
+            dataType: "json",
+            success: function(res){
+                for(var i = 0 ; i < res.items.length ; i++){
+                    var item = res.items[i];
+                    $(".wrap-slider-news__slider-container").slick("slickAdd", 
+                        templateSlider(item.nid,item.uri.split("public://")[1], item.publish_date, item.title));
+                    
+                    $("#loopArticle").append(templateArticle(item.nid, item.title, item.publish_date, item.body))
+                }
+                isLoading = false;
+                pageNumber++;
+                
+            },
+        });
+    }
+});
+
 
 $(".wrap-slider-news__button-icon").on("click" , function(){
     if ($(this).children().hasClass("fa-angle-left")){
@@ -34,7 +59,7 @@ $(".wrap-slider-news__button-icon").on("click" , function(){
     }
 });
 
-$(".wrap-slider-news__slider-image-container , .wrap-slider-news__slider-description").on("click",function(){
+$(".wrap-slider-news__slider-container").on("click",".wrap-slider-news__slider-image-container , .wrap-slider-news__slider-description",function(){
     var id = $(this).attr("data-id");
     $(".wrap-slider-news__news-detail-container").hide();
     $(".wrap-slider-news__news-detail-container[data-id='" + id +"'] ").show();
@@ -43,3 +68,37 @@ $(".wrap-slider-news__slider-image-container , .wrap-slider-news__slider-descrip
     $(".wrap-slider-news__slider-item-container").removeClass("active");
     $(this).closest(".wrap-slider-news__slider-item-container").addClass("active");
 })
+
+
+function templateSlider(id , image , date, title){
+    // http:////cybersecurityasean.com/sites/default/files/styles/news/
+    return `<div class='wrap-slider-news__slider-item col-xs-12'>
+                <div class='wrap-slider-news__slider-item-container'>
+                    <div class='wrap-slider-news__slider-image-container' data-id='${id}' >
+                        <img class='wrap-slider-news__slider-image' src="http:////cybersecurityasean.com/sites/default/files/styles/news/${image}" />
+                    </div>
+                    <div class='wrap-slider-news__slider-content'>
+                        <div class='wrap-slider-news__slider-date'>
+                            <i class='fa fa-calendar-o'></i> ${date}
+                        </div>
+                        <div class='wrap-slider-news__slider-description' data-id='${id}' >
+                            ${title}
+                        </div>
+                    </div>
+                </div>
+            </div>`
+}
+
+function templateArticle(id, title, date, body){
+    return `<div class='col-xs-12 wrap-slider-news__news-detail-container' data-id='${id}'>
+                <div class='wrap-slider-news__news-detail-title'>
+                    ${title}
+                </div>
+                <div class='wrap-slider-news__news-detail-date'>
+                    <i class='fa fa-calendar-o'></i> ${date}
+                </div>
+                <div class='wrap-slider-news__news-detail-content'>
+                    ${body}
+                </div>
+            </div >`
+}
